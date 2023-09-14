a7fcc1afc2a73b90809a4d6c8ee32104849076ec
J0J0 Todos <jojo@peek-a-boo.at>
2023-09-14 14:15:54 +0200

    A final approach on nav auto-active-switching
    which is working.

--- my.js
+++ my.js
@@ -1,20 +1,21 @@
-const navLinks = document.querySelectorAll('nav a');
-navLinks.forEach(link => {
-  link.addEventListener('click', function(e) {
-    console.log("Adding event listener, callback 'e' is:", e)
-    if (e.target.tagName === 'A') {
-      link.classList.remove('active');
-      console.log("removed active class")
-    }
-  })
-})
-
 function toggleNavActive() {
-  const activePage = window.location.href;
-  navLinks.forEach(link => {
-    // if (link.href.includes(`${activePage}`)) {
-    //   console.log(`${activePage}`);
-    // }
-    console.log(link.classList)
-  })
+  //const activePage = window.location.href;
+  //const navLinks = document.querySelectorAll('nav a');
+  // Get container and and all with class="nav-item" inside the container
+  var navContainer = document.getElementById("navbarNav");
+  var navLinks = navContainer.getElementsByClassName("nav-item");
+  // console.log("navLinks:", navLinks.length);
+
+  // Loop through links and add class "active" to current/clicked anchor tag
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      if (current.length > 0) {
+        // Remove " active" from className string
+        current[0].className = current[0].className.replace(" active","");
+      }
+      //this.className += " active";
+      //this.classList.add("active");
+    });
+  }
 }
