3d2e2ea30cd364099bbf06dd3e0e9972ad10a19c
J0J0 Todos <jojo@peek-a-boo.at>
2023-12-13 19:48:39 +0100

    Basic react-router-dom setup in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+// App entry point
+import App from './App.jsx'
 // Import our SCSS (which contains bootstrap stuff!)
 import './main.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <BrowserRouter /* basename="/app" */>
+    <Routes>
+      <Route path="/" element={<App />}/>
+    </Routes>
+  </BrowserRouter>
 )
