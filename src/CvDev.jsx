e75b6193f29ffb1301b5a37212220aaf5f901550
J0J0 Todos <jojo@peek-a-boo.at>
2023-12-15 16:55:18 +0100

    Leave some ideas on how CV listing could look like

--- src/CvDev.jsx
+++ src/CvDev.jsx
@@ -2,7 +2,21 @@ import Page from './components/Page.jsx'
 
 function CvDev() {
   const content = (
-    <></>
+    <>
+      <p>Use regular bullet points for CV listing?</p>
+      <ul className="" id="">
+        <li className="">
+          Did something, somewhere
+        </li>
+        <li className=""> Did something else, somewhere else
+          <ul className="" id="">
+            <li className="">
+              Sub sub, something
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </>
   )
 
   return (
