8bb8f1789326518b60ad1ad28c9da379bbefc1bc
J0J0 Todos <jojo@peek-a-boo.at>
2023-12-16 11:42:37 +0100

    Add all projects to Exercise and Foss component
    data could catually be derived from a json list. Leave it for now and
    postpone to next refactor.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,20 @@
 function Card(props) {
-  function classes() {
-    const bg = props.bgcolor ? 'bg-' + props.bgcolor : ' ';
-    const text = props.txtcolor ? 'text-' + props.txtcolor : 'text-white';
-    return 'card mb-3' + bg + text;
-  }
 
   return(
-    <div className={classes()} style={{maxWidth: "18rem"}}>
-      <div className="card-header">{props.header}</div>
+    <div className="card mb-3" >
+      {props.header && (<div className="card-header">{props.header}</div>)}
       <div className="card-body">
         {props.title && (<h5 className="card-title">{props.title}</h5>)}
-        {props.text && (<p className="card-text">{props.text}</p>)}
-        {props.body}
-        {props.status && (<div className="card-status">{props.status}</div>)}
+        <p className="card-text">
+          {props.text && (props.text)}
+        </p>
+        <p className="mt-o mb-0">
+          {props.repo && (<a href={props.repo} class="btn btn-outline-secondary">Repo</a>)}
+          {props.run && (<a href={props.run} class="btn btn-secondary">Run</a>)}
+          {props.docs && (<a href={props.run} class="btn btn-secondary">Docs</a>)}
+          {props.website && (<a href={props.website} class="btn btn-dark">Website</a>)}
+        </p>
+          {props.members && (<div className="card-status"><em>Members: {props.members}</em></div>)}
       </div>
 
     </div>
