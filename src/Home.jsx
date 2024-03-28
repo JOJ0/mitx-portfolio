b05a8b1043bddcedc5b8c8426830155f9b246c8d
J0J0 Todos <jojo@peek-a-boo.at>
2024-03-28 15:59:20 +0100

    Move pic top right

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,6 +8,9 @@ function Home() {
     <div class="row">
       <div class="col-12">
         <div>
+          <img class="rounded float-end me-3 w-25" src={portrait01} alt="Portrait Picture" />
+        </div>
+        <div>
           <p class="">
             Hi! I've been a sysadmin in my carreer so far, 15+ years working in enterprise IT. My focus has been servers and virtualization. There was Windows, VMware and a little proprietory UNIX's once, but over the years I ended up with Linux as the main platform I've been managing. Already in 1997 I was tinkering with Linux as my homeserver platform, later on it became the go-to platform of the companies I was working for, thus I got into professionally using it.
           </p>
@@ -15,9 +18,6 @@ function Home() {
           <p>In the last couple of years programming became more and more important in my life: private Python tinkering projects evolved to full-blown <Link to="projects/foss">open-source projects</Link>; corporate "sysadmin doing" developed into Configuration Management with tools like Ansible and Puppet, some Ruby scripting, as well as developing entire command line tools in Python.</p>
 
           <p>All that evolved to a desire to professionally learn "to code" and shift my carreer into full time programming. Currently I'm attending a <Link to="projects/exercise">Full Stack Web Programming</Link> course.</p>
-        <div>
-          <img class="rounded float-end me-3 w-5" src={portrait01} alt="Portrait Picture" />
-        </div>
 
           <p>That said, as an IT guy you'll often be the one who's also managing and coding webpages for companies or privately. Hence I'm experienced in CSS/HTML, server-side programming with Python Wagtail/Django, static site-generators like Jekyll, there was some PHP in the early days of my career and I've even designed Python/Flask based REST APIs. Now I'm expanding my expertise by focusing on the one language that seems to rule the web: JavaScript and its frameworks, all up front <Link to="https://react.dev/">React</Link>, <Link to="https://expressjs.com/">Express</Link> and <Link to="https://reactnative.dev/">React Native</Link>.</p>
         </div>
