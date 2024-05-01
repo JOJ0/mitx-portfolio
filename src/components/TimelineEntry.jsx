b87637598a23f27b5cf06232cc70bfd152251ce7
J0J0 Todos <jojo@peek-a-boo.at>
2024-05-01 10:00:48 +0200

    Add highligh prop in TimelineEntry component

--- src/components/TimelineEntry.jsx
+++ src/components/TimelineEntry.jsx
@@ -3,10 +3,10 @@ function TimelineEntry(props) {
   return(
         <div className="col py-2">
 
-            <div className="card border-success shadow">
+            <div className={props.highlight ? "card border-success shadow" : "card"}>
                 <div className="card-body">
-                    <div className="float-end text-success">{ props.topright }</div>
-                    <h4 className="card-title text-success">{ props.topic }</h4>
+                    <div className={props.highlight ? "float-end text-success" : "float-end text-muted"} >{ props.topright }</div>
+                    <h4 className={props.highlight ? "card-title text-success" : "card-title text-muted"}>{ props.topic }</h4>
                     <p className="card-text">{ props.descr }</p>
                 </div>
             </div>
