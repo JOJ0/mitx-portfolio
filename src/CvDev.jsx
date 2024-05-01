e84554212cfc0e0872b637b284a23df8c2ba0545
J0J0 Todos <jojo@peek-a-boo.at>
2024-05-01 09:23:16 +0200

    Replace static entries on CvDev w/ new component

--- src/CvDev.jsx
+++ src/CvDev.jsx
@@ -1,4 +1,5 @@
-import Page from './components/Page.jsx'
+import Page from './components/Page.jsx';
+import TimelineEntry from './components/TimelineEntry.jsx';
 
 
 function CvDev() {
@@ -23,26 +24,7 @@ function CvDev() {
         </div>
 
 
-        <div className="col py-2">
-
-            <div className="card">
-                <div className="card-body">
-                    <div className="float-end text-muted">Mon, Jan 9th 2021 7:00 AM</div>
-                    <h4 className="card-title text-muted">PHP</h4>
-                    <p className="card-text">text</p>
-                </div>
-            </div>
-        </div>
-
-        <div className="col py-2">
-            <div className="card">
-                <div className="card-body">
-                    <div className="float-end text-muted">Mon, Jan 9th 2021 7:00 AM</div>
-                    <h4 className="card-title text-muted">VBscript</h4>
-                    <p className="card-text">text</p>
-                </div>
-            </div>
-        </div>
+        <TimelineEntry topic="MS-DOS Batch" descr="" topright="" />
 
     </div>
 
@@ -64,46 +46,8 @@ function CvDev() {
         </div>
         
         
-        <div className="col py-2">
-            <div className="card border-success shadow">
-                <div className="card-body">
-                    <div className="float-end text-success">Tue, Jan 10th 2021 8:30 AM</div>
-
-                    <h4 className="card-title text-success">PHP</h4>
-                    <p className="card-text">text</p>
-                    <button className="btn btn-sm btn-outline-secondary" type="button" data-bs-target="#t2_details" data-bs-toggle="collapse">Show Details ▼</button>
-                    <div className="collapse border" id="t2_details">
-                        <div className="p-2 text-monospace">
-                            <div>08:30 - 09:00 Breakfast in CR 2A</div>
-                            <div>09:00 - 10:30 Live sessions in CR 3</div>
-                            <div>10:30 - 10:45 Break</div>
-                            <div>10:45 - 12:00 Live sessions in CR 3</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div className="col py-2">
-            <div className="card">
-                <div className="card-body">
-                    <div className="float-end text-muted">Mon, Jan 9th 2021 7:00 AM</div>
-                    <h4 className="card-title text-muted">VBscript</h4>
-                    <p className="card-text">text</p>
-                </div>
-            </div>
-        </div>
-
-        <div className="col py-2">
-            <div className="card">
-                <div className="card-body">
-                    <div className="float-end text-muted">Mon, Jan 9th 2021 7:00 AM</div>
-                    <h4 className="card-title text-muted">Bash</h4>
-                    <p className="card-text">text</p>
-                </div>
-            </div>
-        </div>
-
+        <TimelineEntry topic="MS-DOS Batch" descr="" topright="" />
+        <TimelineEntry topic="Bash" descr="" topright="" />
 
     </div>
 
@@ -123,15 +67,9 @@ function CvDev() {
             </div>
         </div>
 
-        <div className="col py-2">
-            <div className="card">
-                <div className="card-body">
-                    <div className="float-end text-muted">Thu, Jan 12th 2021 11:30 AM</div>
-                    <h4 className="card-title">Day 4 Wrap-up</h4>
-                    <p className="card-text">text</p>
-                </div>
-            </div>
-        </div>
+        <TimelineEntry topic="MS-DOS Batch" descr="" topright="" />
+        <TimelineEntry topic="Bash" descr="" topright="" />
+
     </div>
 
 </div>
