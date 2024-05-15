d3190c548862e2224d62d1e643b49dd8339cc6e6
J0J0 Todos <2733783+JOJ0@users.noreply.github.com>
2024-05-15 09:45:12 +0200

    Update CvDev.jsx

--- src/CvDev.jsx
+++ src/CvDev.jsx
@@ -1,7 +1,7 @@
 import Page from './components/Page.jsx';
 import TimelineEntry from './components/TimelineEntry.jsx';
 import TimelineBullet from './components/TimelineBullet.jsx';
-import logo_microsoft from '~assets/microsoft.svg';
+import logo_ms from '~assets/microsoft.svg';
 
 
 function CvDev() {
@@ -9,12 +9,12 @@ function CvDev() {
   <>
     <div className="row">
         <TimelineBullet caption="1995-1997" />
-        <TimelineEntry topic="MS-DOS Batch" descr="" topright="" image={ logo_microsoft } />
+        <TimelineEntry topic="MS-DOS Batch" descr="" topright="" image={ logo_ms } />
     </div>
 
     <div className="row">
         <TimelineBullet caption="1998-1999" highlight={false} />
-        <TimelineEntry topic="MS-DOS Batch" descr="" topright="" highlight={false} />
+        <TimelineEntry topic="MS-DOS Batch" descr="" topright="" image={ logo_ms } highlight={false} />
         <TimelineEntry topic="Bash" descr="" topright="" highlight={false} />
         <TimelineEntry topic="HTML/CSS" descr="" topright="" highlight={false} />
     </div>
