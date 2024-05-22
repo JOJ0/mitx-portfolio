788d4be38dc7af3eec3e494c39076e8d3dc9ec80
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-05-22 09:42:41 +0200

    cvdev fix indent

--- src/components/TimelineEntry.jsx
+++ src/components/TimelineEntry.jsx
@@ -1,17 +1,16 @@
 function TimelineEntry(props) {
 
   return(
-        <div className="col py-2">
-
-            <div className={props.highlight ? "card border-success shadow" : "card"}>
-                <div className="card-body">
-                    <div className={props.highlight ? "float-end text-success" : "float-end text-muted"} >{ props.topright }</div>
-                    <h4 className={props.highlight ? "card-title text-success" : "card-title text-muted"}>{ props.topic }</h4>
-                    <p className="card-text">{ props.descr }</p>
-                    <img className="img-fluid" src={ props.image } />
-                </div>
-            </div>
+    <div className="col py-2">
+      <div className={props.highlight ? "card border-success shadow" : "card"}>
+        <div className="card-body">
+          <div className={props.highlight ? "float-end text-success" : "float-end text-muted"} >{ props.topright }</div>
+            <h4 className={props.highlight ? "card-title text-success" : "card-title text-muted"}>{ props.topic }</h4>
+            <p className="card-text">{ props.descr }</p>
+            <img className="img-fluid" src={ props.image } />
+          </div>
         </div>
+    </div>
   )
 }
 
