ec3b3ce5e7f512d32c352983df8f3938d5a05ee3
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-05-22 09:50:24 +0200

    timeline entry set img dimensions 32x32 in html
    and fix a space elsewhere

--- src/components/TimelineEntry.jsx
+++ src/components/TimelineEntry.jsx
@@ -4,10 +4,10 @@ function TimelineEntry(props) {
     <div className="col py-2">
       <div className={props.highlight ? "card border-success shadow" : "card"}>
         <div className="card-body">
-          <div className={props.highlight ? "float-end text-success" : "float-end text-muted"} >{ props.topright }</div>
+          <div className={props.highlight ? "float-end text-success" : "float-end text-muted"}>{ props.topright }</div>
             <h4 className={props.highlight ? "card-title text-success" : "card-title text-muted"}>{ props.topic }</h4>
             <p className="card-text">{ props.descr }</p>
-            <img className="img-fluid" src={ props.image } />
+            <img className="img-fluid" src={ props.image } width="32" height="32" />
           </div>
         </div>
     </div>
