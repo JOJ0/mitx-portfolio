24437eafbdea340dcdbf6d3158ee1a260f4c3839
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-05-22 09:08:10 +0200

    cvdev rename dos batch

--- src/CvDev.jsx
+++ src/CvDev.jsx
@@ -9,12 +9,13 @@ function CvDev() {
   <>
     <div className="row">
         <TimelineBullet caption="1995-1997" />
-        <TimelineEntry topic="MS-DOS Batch" descr="" topright="" image={ logo_ms } />
+        <TimelineEntry topic="DOS BATCH" descr="" topright="" image={ logo_ms } />
+        <TimelineEntry topic="GWBASIC" descr="" topright="" image={ logo_ms } />
     </div>
 
     <div className="row">
         <TimelineBullet caption="1998-1999" highlight={false} />
-        <TimelineEntry topic="MS-DOS Batch" descr="" topright="" image={ logo_ms } highlight={false} />
+        <TimelineEntry topic="DOS Batch" descr="" topright="" image={ logo_ms } highlight={false} />
         <TimelineEntry topic="Bash" descr="" topright="" highlight={false} />
         <TimelineEntry topic="HTML/CSS" descr="" topright="" highlight={false} />
     </div>
