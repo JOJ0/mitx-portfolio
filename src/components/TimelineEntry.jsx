901dda30797e057e3c69df8427d6286a7ec100c9
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-05-23 06:48:22 +0200

    timeline entry fix closing tag

--- src/components/TimelineEntry.jsx
+++ src/components/TimelineEntry.jsx
@@ -5,7 +5,7 @@ function TimelineEntry(props) {
       <div className={props.highlight ? "card border-success shadow" : "card"}>
         <div className="card-body">
           <div className={props.highlight ? "float-end text-success" : "float-end text-muted"}>{ props.topright }</div>
-            <h6 className={props.highlight ? "card-title text-success" : "card-title text-muted"}>{ props.topic }</h4>
+            <h6 className={props.highlight ? "card-title text-success" : "card-title text-muted"}>{ props.topic }</h6>
             <p className="card-text">{ props.descr }</p>
             <img className="" src={ props.image } width="32" height="32" />
           </div>
