2fea206eafad470704511e5d7685e0ac34d45a80
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-05-23 07:34:22 +0200

    css tl entry move image to top off card

--- src/components/TimelineEntry.jsx
+++ src/components/TimelineEntry.jsx
@@ -3,11 +3,11 @@ function TimelineEntry(props) {
   return(
     <div className="col py-2">
       <div className={props.highlight ? "card border-success shadow" : "card"}>
+        <img className="" src={ props.image } width="32" height="32" />
         <div className="card-body">
           <div className={props.highlight ? "float-end text-success" : "float-end text-muted"}>{ props.topright }</div>
             <h6 className={props.highlight ? "card-title text-success" : "card-title text-muted"}>{ props.topic }</h6>
             <p className="card-text">{ props.descr }</p>
-            <img className="" src={ props.image } width="32" height="32" />
           </div>
         </div>
     </div>
