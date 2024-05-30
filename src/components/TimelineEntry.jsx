63437ac23111b13b6118dd44656b44cb3e5d319d
J0J0 Todos <jojo@peek-a-boo.at>
2024-05-30 15:29:27 +0200

    Revert "Debug enable borders on CvDev"
    This reverts commit 4b61cbc407221e036867f8730a3a73a7e8bb7e87.

--- src/components/TimelineEntry.jsx
+++ src/components/TimelineEntry.jsx
@@ -1,10 +1,10 @@
 function TimelineEntry(props) {
 
   return(
-    <div className="col-auto py-2 border">
+    <div className="col-auto py-2">
       <div className={props.highlight ? "card card-timeline border-success shadow" : "card card-timeline"}>
         <img className="card-img-top" src={ props.image } width="32" height="32" />
-        <div className="card-body p-2 border">
+        <div className="card-body p-2">
           <div className={props.highlight ? "float-end text-success" : "float-end text-muted"}>{ props.topright }</div>
             <h6 className={props.highlight ? "card-title card-title-timeline text-success text-center" : "card-title card-title-timeline text-muted text-center"}>{ props.topic }</h6>
           </div>
