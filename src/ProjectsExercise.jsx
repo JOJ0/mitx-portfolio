7da11ca6697c68a551cb9a5ba0c95b2375e93a64
J0J0 Todos <jojo@peek-a-boo.at>
2024-06-13 10:48:27 +0200

    Refactor proj/exercise to read data from json file
    and prepare datafile for foss and tools project.

--- src/ProjectsExercise.jsx
+++ src/ProjectsExercise.jsx
@@ -1,59 +1,37 @@
 import Page from './components/Page.jsx'
 import Card from './components/Card.jsx'
+import projects from './db_projects.json'
+
 
 function ProjectsExercise() {
-  const title = "Projects.Exercise"
+  const title = "Projects.Exercise";
+  const Cols = () => {
+    const cols = projects.exercise.map((col, index) => {
+      return (
+        <div key={index} className="col-6 col-sm-4 col-md-3">
+          <Card
+            title={col.title}
+            text={col.text}
+            repo={col.repo}
+            run={col.run}
+            language={col.language}
+          />
+        </div>
+      );
+    });
+    return cols;
+  };
+
   const content = (
     <>
     <div className="row">
       <p>
-        In 2023/2024 I'm attending the web programming course <a href="https://xpro.mit.edu/courses/course-v1:xPRO+PCCx/">Professional Certificate in Coding: Full Stack Development with MERN</a> at <a href="http://xpro.mit.edu">MITx Pro</a>. Most projects on this page were initiated during this course.
+        In 2023/2024 I attended the web programming course <a href="https://xpro.mit.edu/courses/course-v1:xPRO+PCCx/">Professional Certificate in Coding: Full Stack Development with MERN</a> at <a href="http://xpro.mit.edu">MITx Pro</a>. Most projects on this page were initiated during this course.
       </p>
     </div>
 
     <div className="row">
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="PacMen Exercise"
-          text="Generates PacMen that auto-move around the screen."
-          repo="http://github.com/JOJ0/mitx-pacmen"
-          run="https://joj0.github.io/mitx-pacmen"
-          language="JavaScript/HTML/CSS"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="Eye Exercise"
-          text="Like Xwindows Xeyes, but in the web browser"
-          repo="http://github.com/JOJ0/mitx-eye"
-          run="https://joj0.github.io/mitx-eye"
-          docs=""
-          website=""
-          language="JavaScript/HTML/CSS"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="Real Time Bus Tracker Exercise"
-          text="Watch where busses currently are driving in Boston/Massachusetts on an embedded Google Map."
-          repo="http://github.com/JOJ0/mitx-bustracker"
-          docs="https://github.com/JOJ0/mitx-bustracker#how-to-run"
-          run=""
-          website=""
-          language="JavaScript/HTML/CSS"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="Bad Bank Exercise"
-          text="Using React components to build a website for an imaginary bank"
-          repo="http://github.com/JOJ0/mitx-badbank"
-          run=""
-          docs=""
-          website=""
-          language="ReactJS"
-        />
-      </div>
+      <Cols />
     </div>
     </>
   );
