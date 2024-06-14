1ac29b69f3c5b222c4a28cf76f65e4d2c04a5be8
J0J0 Todos <jojo@peek-a-boo.at>
2024-06-14 09:51:29 +0200

    Refactor proj/tool, proj/foss to use json file

--- src/ProjectsFoss.jsx
+++ src/ProjectsFoss.jsx
@@ -1,5 +1,7 @@
 import Page from './components/Page.jsx'
-import Card from './components/Card.jsx'
+import { Card, CardColumns } from './components/Card.jsx'
+import projects from './db_projects.json'
+
 
 function ProjectsFoss() {
   const title = "Projects.FOSS"
@@ -11,77 +13,7 @@ function ProjectsFoss() {
       </p>
     </div>
     <div className="row">
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="synadm"
-          text="The Admin CLI for Synapse (a communication server based on the Matrix specification)"
-          repo="https://github.com/JOJ0/synadm"
-          run=""
-          docs="https://synadm.readthedocs.io"
-          website=""
-          members="3"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="python3-discogs-client"
-          text="The Python library for accessing the Discogs API (the worlds [probably] largest free online database and marketplace of music releases"
-          repo="https://github.com/joalla/discogs_client"
-          run=""
-          docs="https://python3-discogs-client.readthedocs.io"
-          website=""
-          language="Python"
-          members="4"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="beets"
-          text="A sophsticated media file tagger and music library manager written in Python"
-          repo="https://github.com/beetbox/beets"
-          run=""
-          docs="https://beets.readthedocs.io/en/latest"
-          website="https://beets.io"
-          language="Python"
-          members="30+"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="DiscoDOS"
-          text="A CLI tool for DJ's and record collectors based on the discogs.com collection feature"
-          repo="https://github.com/JOJ0/discodos"
-          run=""
-          docs="https://discodos.readthedocs.io"
-          website="https://discodos.jojotodos.net"
-          language="Python"
-          members="1"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="beets-xtractor"
-          text="A beets plugin based on the Essentia project of Barcelona's Universitat Pompeu Fabra"
-          repo="https://github.com/adamjakab/BeetsPluginXtractor"
-          run=""
-          docs="https://github.com/adamjakab/BeetsPluginXtractor/blob/master/README.md"
-          website=""
-          language="Python"
-          members="2"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="beets-dirfields"
-          text="A beets plugin that remembers where files where originally stored on disk"
-          repo="https://github.com/JOJ0/beets-dirfields"
-          run=""
-          docs="https://github.com/JOJ0/beets-dirfields/blob/master/README.md"
-          website=""
-          language="Python"
-          members="1"
-        />
-      </div>
+      <CardColumns cardsList={projects["foss"]}/>
     </div>
     </>
   );
--- src/ProjectsTool.jsx
+++ src/ProjectsTool.jsx
@@ -1,5 +1,7 @@
 import Page from './components/Page.jsx'
-import Card from './components/Card.jsx'
+import { Card, CardColumns } from './components/Card.jsx'
+import projects from './db_projects.json'
+
 
 function ProjectsTool() {
   const title = "Projects.Tool"
@@ -12,68 +14,7 @@ function ProjectsTool() {
     </div>
 
     <div className="row">
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="Jekyll Helpers"
-          text="Helpers to generate picture galleries for Jekyll based websites/blogs"
-          repo="https://github.com/JOJ0/jekyll-helpers"
-          run=""
-          language="Bash/Zsh/Python"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="Transaction Cutter"
-          text="Cuts out the relevant pieces of transaction CSV files of a certain bank or PayPal - my first Python project ever :-O"
-          repo="https://github.com/JOJ0/transaction-cutter"
-          run=""
-          language="Python"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="Audio Utils"
-          text="Shell scripts to mass-manipulate audio and video files"
-          repo="https://github.com/JOJ0/audio-utils"
-          run=""
-          language="Bash/Zsh"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="Mailchimp Members"
-          text="Unpacks a mailchimp members list zip file, filters columns and writes new csv file"
-          repo="https://github.com/JOJ0/mailchimp-members"
-          run=""
-          language="Python"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="GitHub Projects Util"
-          text='Uses PyGithub to retrieve "GitHub Project" (aka kanban) cards'
-          repo="https://github.com/JOJ0/github-util"
-          run=""
-          language="Python"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title="MPC 1000 Sequence Util"
-          text="CLI utility to manipulate sequence files of an AKAI MPC 1000 sampler device"
-          repo="https://github.com/JOJ0/mpc1k-seq"
-          run=""
-          language="Python"
-        />
-      </div>
-      <div className="col-6 col-sm-4 col-md-3">
-        <Card
-          title='APC mini Controller Script'
-          text='Ableton Live 9.x controller script for the "Akai MPC mini"'
-          repo="https://github.com/JOJ0/ableton-live9-remote-scripts"
-          language="Python"
-        />
-      </div>
+      <CardColumns cardsList={projects["tool"]}/>
     </div>
     </>
   );
