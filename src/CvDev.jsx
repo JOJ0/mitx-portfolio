1fa99ceb7509ad902dc4ae0f0d53c2b4a10625bc
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-06-23 09:11:27 +0200

    Fox typos in cvdev

--- src/CvDev.jsx
+++ src/CvDev.jsx
@@ -8,12 +8,10 @@ function CvDev() {
   <>
     <TimelineRow data={cv["dev"]} year="1994" />
     <TimelineRow data={cv["dev"]} year="1995-1997" />
-    <TimelineRow data={cv["dev"]} year="1998"
-    <TimelineRow data={cv["dev"]} year="1999-2001"
-    <TimelineRow data={cv["dev"]} year="2002-2004"
-    <TimelineRow data={cv["dev"]} year="2005-2006"
-  
-  
+    <TimelineRow data={cv["dev"]} year="1998" />
+    <TimelineRow data={cv["dev"]} year="1999-2001" />
+    <TimelineRow data={cv["dev"]} year="2002-2004" />
+    <TimelineRow data={cv["dev"]} year="2005-2006" />
   
   
   
