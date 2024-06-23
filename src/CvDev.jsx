689c553b6cb18033598d06c0ccdb6be74f7a9343
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-06-23 09:05:21 +0200

    Add cvdev timeline rows

--- src/CvDev.jsx
+++ src/CvDev.jsx
@@ -6,8 +6,17 @@ import cv from './db_cv.json';
 function CvDev() {
   const content = (
   <>
+    <TimelineRow data={cv["dev"]} year="1994" />
     <TimelineRow data={cv["dev"]} year="1995-1997" />
-    <TimelineRow data={cv["dev"]} year="1998-1999" />
+    <TimelineRow data={cv["dev"]} year="1998"
+    <TimelineRow data={cv["dev"]} year="1999-2001"
+    <TimelineRow data={cv["dev"]} year="2002-2004"
+    <TimelineRow data={cv["dev"]} year="2005-2006"
+  
+  
+  
+  
+  
   </>
   )
 
