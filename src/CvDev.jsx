8c39c8631fa6a7d504de6a29acf44b0d8573f13a
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-06-24 13:32:09 +0200

    Input db cvdev data and fix rows definitions

--- src/CvDev.jsx
+++ src/CvDev.jsx
@@ -8,9 +8,9 @@ function CvDev() {
   <>
     <TimelineRow data={cv["dev"]} year="1994" />
     <TimelineRow data={cv["dev"]} year="1995-1997" />
-    <TimelineRow data={cv["dev"]} year="1998" />
-    <TimelineRow data={cv["dev"]} year="1999-2001" />
-    <TimelineRow data={cv["dev"]} year="2002-2004" />
+    <TimelineRow data={cv["dev"]} year="1998-2000" />
+    <TimelineRow data={cv["dev"]} year="2002" />
+    <TimelineRow data={cv["dev"]} year="2003-2004" />
     <TimelineRow data={cv["dev"]} year="2005-2006" />
   
   
