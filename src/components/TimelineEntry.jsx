d568d51728e6c55ddbf11d0790f5a82dd918d7bc
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-06-25 09:17:56 +0200

    Fix jsx in tl entry

--- src/components/TimelineEntry.jsx
+++ src/components/TimelineEntry.jsx
@@ -20,7 +20,7 @@ export function TimelineEntriesFactory(props) {
   const sorted = props.entries.sort((a, b) => a.topic.localeCompare(b.topic));
   const entries = sorted.map((entry, index) => {
     return (
-      <div className="card-deck"></>
+      <div className="card-deck">
       <TimelineEntry topic={entry.topic} image={entry.icon} key={index}/>
       </div>
     );
