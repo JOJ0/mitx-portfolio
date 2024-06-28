0a4bfdd97bce804c47e4412a2b361af5d6fe6f3e
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-06-28 08:14:19 +0200

    Add tl rows to cvdev

--- src/CvDev.jsx
+++ src/CvDev.jsx
@@ -9,12 +9,20 @@ function CvDev() {
     <TimelineRow data={cv["dev"]} year="1994" />
     <TimelineRow data={cv["dev"]} year="1995-1997" />
     <TimelineRow data={cv["dev"]} year="1998-2000" />
+    <TimelineRow data={cv["dev"]} year="2001" />
     <TimelineRow data={cv["dev"]} year="2002" />
     <TimelineRow data={cv["dev"]} year="2003-2004" />
     <TimelineRow data={cv["dev"]} year="2005-2006" />
-  
-  
-  
+    <TimelineRow data={cv["dev"]} year="2007" />
+    <TimelineRow data={cv["dev"]} year="2008" />
+    <TimelineRow data={cv["dev"]} year="2009-2014" />
+    <TimelineRow data={cv["dev"]} year="2015-2017" />
+    <TimelineRow data={cv["dev"]} year="2018-2019" />
+    <TimelineRow data={cv["dev"]} year="2020" />
+    <TimelineRow data={cv["dev"]} year="2021" />
+    <TimelineRow data={cv["dev"]} year="2022" />
+    <TimelineRow data={cv["dev"]} year="2023-2024" />
+
   </>
   )
 
