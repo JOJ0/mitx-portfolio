6ada6c070bd824bf557c620f56753f353230f981
J0J0 Todos <jojo@peek-a-boo.at>
2024-07-05 10:01:15 +0200

    Replace literal TimelineRow in cvdev with for loop

--- src/CvDev.jsx
+++ src/CvDev.jsx
@@ -4,45 +4,14 @@ import cv from './db_cv.json';
 
 
 function CvDev() {
-  const content = (
-  <>
-    <TimelineRow data={cv["dev"]} year="1994" />
-    <TimelineRow data={cv["dev"]} year="1995" />
-    <TimelineRow data={cv["dev"]} year="1996" />
-    <TimelineRow data={cv["dev"]} year="1997" />
-    <TimelineRow data={cv["dev"]} year="1998" />
-    <TimelineRow data={cv["dev"]} year="1999" />
-    <TimelineRow data={cv["dev"]} year="2000" />
-    <TimelineRow data={cv["dev"]} year="2001" />
-    <TimelineRow data={cv["dev"]} year="2002" />
-    <TimelineRow data={cv["dev"]} year="2003" />
-    <TimelineRow data={cv["dev"]} year="2004" />
-    <TimelineRow data={cv["dev"]} year="2005" />
-    <TimelineRow data={cv["dev"]} year="2006" />
-    <TimelineRow data={cv["dev"]} year="2007" />
-    <TimelineRow data={cv["dev"]} year="2008" />
-    <TimelineRow data={cv["dev"]} year="2009" />
-    <TimelineRow data={cv["dev"]} year="2010" />
-    <TimelineRow data={cv["dev"]} year="2011" />
-    <TimelineRow data={cv["dev"]} year="2012" />
-    <TimelineRow data={cv["dev"]} year="2013" />
-    <TimelineRow data={cv["dev"]} year="2014" />
-    <TimelineRow data={cv["dev"]} year="2015" />
-    <TimelineRow data={cv["dev"]} year="2016" />
-    <TimelineRow data={cv["dev"]} year="2017" />
-    <TimelineRow data={cv["dev"]} year="2018" />
-    <TimelineRow data={cv["dev"]} year="2019" />
-    <TimelineRow data={cv["dev"]} year="2020" />
-    <TimelineRow data={cv["dev"]} year="2021" />
-    <TimelineRow data={cv["dev"]} year="2022" />
-    <TimelineRow data={cv["dev"]} year="2023" />
-    <TimelineRow data={cv["dev"]} year="2024" />
-
-  </>
-  )
+  const years = ["1994","1995","1996"];
+  const rows = [];
+  for (var y=1994; y < 2024; y++) {
+    rows.push(<TimelineRow data={cv["dev"]} year={y} />);
+  }
 
   return (
-    <Page title="CV.dev" content={content} />
+    <Page title="CV.dev" content={rows} />
   )
 }
 
