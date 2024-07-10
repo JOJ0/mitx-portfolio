b7bbe57563fac2f13b6f1017a590ca6b1756d7b7
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-07-10 20:36:44 +0200

    Try h1 font for tl bullet year

--- src/components/TimelineBullet.jsx
+++ src/components/TimelineBullet.jsx
@@ -9,9 +9,9 @@ export function TimelineBullet(props) {
                 <div className="col">&nbsp;</div>
             </div>
 
-            <h3 className="m-2">
+            <h1 className="m-2">
                 <span className={hl ? "badge rounded-pill bg-success" : "badge rounded-pill bg-dark"}>{props.caption}</span>
-            </h3>
+            </h1>
 
             <div className="row h-50">
                 <div className="col border-end order">&nbsp;</div>
