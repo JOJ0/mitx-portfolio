1314d32d74caa70baea449f334bc314ee9ce7aba
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-07-10 14:45:55 +0200

    cvdev fix max year

--- src/CvDev.jsx
+++ src/CvDev.jsx
@@ -6,7 +6,7 @@ import cv from './db_cv.json';
 function CvDev() {
   const years = ["1994","1995","1996"];
   const rows = [];
-  for (var y=1994; y < 2024; y++) {
+  for (var y=1994; y <= 2024; y++) {
     rows.push(<TimelineRow data={cv["dev"]} year={y} />);
   }
 
