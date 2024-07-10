ab3a4dac2c5ec4d605627f54b9674946e5aab986
J0J0 Todos <JOJ0@users.noreply.github.com>
2024-07-10 15:02:16 +0200

    Get started with structured cvops and db cv
    and a first entry debian

--- src/CvOps.jsx
+++ src/CvOps.jsx
@@ -1,40 +1,16 @@
-import Page from './components/Page.jsx'
+import Page from './components/Page.jsx';
+import { TimelineRow } from './components/TimelineRow.jsx';
+import cv from './db_cv.json';
+
 
 function CvOps() {
-  const content = (
-    <>
-      <p>Use list-group bootstrap thing for CV listing?</p>
-      <div className="list-group">
-        <a href="#" className="list-group-item list-group-item-action" aria-current="true">
-          <div className="d-flex w-100 justify-content-between">
-            <h5 className="mb-1">List group item heading</h5>
-            <small>3 days ago</small>
-          </div>
-          <p className="mb-1">Some placeholder content in a paragraph.</p>
-          <small>And some small print.</small>
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          <div className="d-flex w-100 justify-content-between">
-            <h5 className="mb-1">List group item heading</h5>
-            <small className="text-muted">3 days ago</small>
-          </div>
-          <p className="mb-1">Some placeholder content in a paragraph.</p>
-          <small className="text-muted">And some muted small print.</small>
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          <div className="d-flex w-100 justify-content-between">
-            <h5 className="mb-1">List group item heading</h5>
-            <small className="text-muted">3 days ago</small>
-          </div>
-          <p className="mb-1">Some placeholder content in a paragraph.</p>
-          <small className="text-muted">And some muted small print.</small>
-        </a>
-      </div>
-    </>
-  )
+  const rows = [];
+  for (var y=1994; y <= 2024; y++) {
+    rows.push(<TimelineRow data={cv["ops"]} year={y} />);
+  }
 
   return (
-    <Page title="CV.ops" content={content} />
+    <Page title="CV.ops" content={rows} />
   )
 }
 
