c61ece019095bf009902027e3f6867f9ffe72416
J0J0 Todos <jojo@peek-a-boo.at>
2024-07-22 21:01:50 +0200

    Replace Bio but leave some WIP coming soon links

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,16 +10,36 @@ function Home() {
         <div>
           <img class="img-fluid rounded float-end me-3 portrait-max" src={portrait01} alt="Portrait Picture" />
         </div>
+
         <div>
-          <p class="">
-            Hi! I've been a sysadmin in my carreer so far, 15+ years working in enterprise IT. My focus has been servers and virtualization. There was Windows, VMware and a little proprietory UNIX's once, but over the years I ended up with Linux as the main platform I've been managing. Already in 1997 I was tinkering with Linux as my homeserver platform, later on it became the go-to platform of the companies I was working for, thus I got into professionally using it.
+          <p>
+            Hi, I'm Jojo. I develop and help maintain Python Open Source software. I have about 10 years experience of Python programming and about 5 in project maintaining, reviewing code and taking care of project communities. In my last job I wrote a Python-based toolset that interacts with in-house API's to help install and orchestrate KVM Virtual Machines.
+          </p>
+          <p>
+            I also code websites. Currently I would use a modern approach using JavaScript, Bootstrap and React.js for that. Express.js if a JavaScript backend is desired. In the past I've coded and still maintain Python based web solutions (Flask, Django, Wagtail).
+          </p>
+          <p>
+            I have a senior/master level background in enterprise system administration of all kinds of (mostly) Open Source based solutions.
+          </p>
+          <p>
+            Since DevOps and automation became popular in my last jobs, I had the chance to focus on that and became quite good in using CI/CD, Puppet, Ansible, Docker and even further improve my Python skills. "Infrastructure as Code Developer" was the buzzwordy title I earned last.
+          </p>
+          <p>
+            If something's not tracked in version control it's not existing!
+          </p>
+          <p>
+            Get an overview of <Link to="projects/foss">the FOSS projects I'm involved with here</Link>, and find out what <Link to="projects/corporate">corporate (closed source) projects I'm proud of there - WIP coming soon</Link>.
+          </p>
+          <p>
+            An overview of my programming skillset is found  <Link to="cvdev">in this alternative approach on a CV</Link>
+          </p>
+          <p>
+            Similarily my ops and sysadmin experience <Link to="cvops">is summarized here - WIP coming soon</Link>
+          </p>
+          <p>
+            Last and surely not least I have some experience in the music and art scene. I've organized <Link to="cvart">music events, DJ nights and participated in art projects - WIP coming soon</Link>.
           </p>
 
-          <p>In the last couple of years programming became more and more important in my life: private Python tinkering projects evolved to full-blown <Link to="projects/foss">open-source projects</Link>; corporate "sysadmin doing" developed into Configuration Management with tools like Ansible and Puppet, some Ruby scripting, as well as developing entire command line tools in Python.</p>
-
-          <p>All that evolved to a desire to professionally learn "to code" and shift my carreer into full time programming. Currently I'm attending a <Link to="projects/exercise">Full Stack Web Programming</Link> course.</p>
-
-          <p>That said, as an IT guy you'll often be the one who's also managing and coding webpages for companies or privately. Hence I'm experienced in CSS/HTML, server-side programming with Python Wagtail/Django, static site-generators like Jekyll, there was some PHP in the early days of my career and I've even designed Python/Flask based REST APIs. Now I'm expanding my expertise by focusing on the one language that seems to rule the web: JavaScript and its frameworks, all up front <Link to="https://react.dev/">React</Link>, <Link to="https://expressjs.com/">Express</Link> and <Link to="https://reactnative.dev/">React Native</Link>.</p>
         </div>
       </div>
     </div>
