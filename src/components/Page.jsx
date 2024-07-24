cf2eae078c3233b427f5b527237f5f89ddadd34d
J0J0 Todos <jojo@peek-a-boo.at>
2024-07-24 11:08:17 +0200

    Page col instead of col-sm-12 (same)

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -4,7 +4,7 @@ function Page({title, subheading, content}) {
   return (
     <>
       <div className="row">
-        <div className="col-sm-12">
+        <div className="col">
           <header>
             <h1 className="font-weight-light text-muted py-3">
               {title} <small className="smaller" >{subheading}</small>
