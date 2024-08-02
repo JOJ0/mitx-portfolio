d67033c7e323c164fa7a070f74499aff545da3ce
J0J0 Todos <jojo@peek-a-boo.at>
2024-08-02 12:59:42 +0200

    Fix nav breakpoint to md instead of lg

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,7 +5,7 @@ import logo_jt_svg from '~assets/jt_noframe_bigger02_grey80_NoBack_center.svg'
 function Nav() {
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-md navbar-light bg-light">
 
       <div className="container-sm">
 
@@ -20,12 +20,12 @@ function Nav() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav" id="navbarList">
             <li className="nav-item">
-              <NavLink className="nav-link d-lg-none" to="/">
+              <NavLink className="nav-link d-md-none" to="/">
                 <span data-bs-toggle="collapse" data-bs-target="#navbarNav">
                   Bio
                 </span>
               </NavLink>
-              <NavLink className="nav-link d-none d-lg-block" to="/">
+              <NavLink className="nav-link d-none d-md-block" to="/">
                   Bio
               </NavLink>
             </li>
@@ -37,28 +37,28 @@ function Nav() {
 
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                 <li>
-                  <NavLink className="nav-link d-lg-none" to="/projects/foss">
+                  <NavLink className="nav-link d-md-none" to="/projects/foss">
                     <span data-bs-toggle="collapse" data-bs-target="#navbarNav">
                       Open-source
                     </span>
                   </NavLink>
-                  <NavLink className="nav-link d-none d-lg-block" to="/projects/foss">
+                  <NavLink className="nav-link d-none d-md-block" to="/projects/foss">
                       Open-source
                   </NavLink>
-                  <NavLink className="nav-link d-lg-none" to="/projects/exercise">
+                  <NavLink className="nav-link d-md-none" to="/projects/exercise">
                     <span data-bs-toggle="collapse" data-bs-target="#navbarNav">
                       Exercise
                     </span>
                   </NavLink>
-                  <NavLink className="nav-link d-none d-lg-block" to="/projects/exercise">
+                  <NavLink className="nav-link d-none d-md-block" to="/projects/exercise">
                       Exercise
                   </NavLink>
-                  <NavLink className="nav-link d-lg-none" to="/projects/tool">
+                  <NavLink className="nav-link d-md-none" to="/projects/tool">
                     <span data-bs-toggle="collapse" data-bs-target="#navbarNav">
                       Tool
                     </span>
                   </NavLink>
-                  <NavLink className="nav-link d-none d-lg-block" to="/projects/tool">
+                  <NavLink className="nav-link d-none d-md-block" to="/projects/tool">
                       Tool
                   </NavLink>
                 </li>
@@ -72,28 +72,28 @@ function Nav() {
 
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                 <li>
-                  <NavLink className="nav-link d-lg-none" to="/cvdev">
+                  <NavLink className="nav-link d-md-none" to="/cvdev">
                     <span data-bs-toggle="collapse" data-bs-target="#navbarNav">
                       CV.dev
                     </span>
                   </NavLink>
-                  <NavLink className="nav-link d-none d-lg-block" to="/cvdev">
+                  <NavLink className="nav-link d-none d-md-block" to="/cvdev">
                       CV.dev
                   </NavLink>
-                  <NavLink className="nav-link d-lg-none" to="/cvops">
+                  <NavLink className="nav-link d-md-none" to="/cvops">
                     <span data-bs-toggle="collapse" data-bs-target="#navbarNav">
                       CV.ops
                     </span>
                   </NavLink>
-                  <NavLink className="nav-link d-none d-lg-block" to="/cvops">
+                  <NavLink className="nav-link d-none d-md-block" to="/cvops">
                       CV.ops
                   </NavLink>
-                  <NavLink className="nav-link d-lg-none" to="/cvart">
+                  <NavLink className="nav-link d-md-none" to="/cvart">
                     <span data-bs-toggle="collapse" data-bs-target="#navbarNav">
                       CV.art
                     </span>
                   </NavLink>
-                  <NavLink className="nav-link d-none d-lg-block" to="/cvart">
+                  <NavLink className="nav-link d-none d-md-block" to="/cvart">
                       CV.art
                   </NavLink>
                 </li>
@@ -101,12 +101,12 @@ function Nav() {
             </li>
 
             <li className="nav-item">
-              <NavLink className="nav-link d-lg-none" to="/about">
+              <NavLink className="nav-link d-md-none" to="/about">
                 <span data-bs-toggle="collapse" data-bs-target="#navbarNav">
                   Why
                 </span>
               </NavLink>
-              <NavLink className="nav-link d-none d-lg-block" to="/about">
+              <NavLink className="nav-link d-none d-md-block" to="/about">
                   Why
               </NavLink>
             </li>
