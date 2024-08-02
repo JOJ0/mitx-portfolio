298830f485c636594360552a661354f9977488fe
J0J0 Todos <jojo@peek-a-boo.at>
2024-08-02 12:10:07 +0200

    Remove debug msgs from TimelineRow

--- src/components/TimelineRow.jsx
+++ src/components/TimelineRow.jsx
@@ -3,17 +3,10 @@ import { TimelineBullet } from './TimelineBullet.jsx';
 
 export const TimelineRow = (props) => {
   const filtered = props.data.filter((entry) => {
-    // return entry.year == props.year;
-    console.log("entry is:");
-    console.log(entry)
-    console.log("props.year is:");
-    console.log(props.year);
-    
     // Handle year as string and int
     if (entry.year.includes(props.year) || entry.year.includes(parseInt(props.year))) {
       return true;
     };
- 
     return false;
   });
 
