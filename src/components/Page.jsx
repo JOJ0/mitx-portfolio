c0b3d03ef55ced1721cd4c24e4233dcb9aabdda9
J0J0 Todos <jojo@peek-a-boo.at>
2026-08-03 23:13:17 +0200

    Move subheading to its own div (keep for mobile)

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -7,11 +7,18 @@ function Page({title, subheading, content}) {
         <div className="col">
           <header>
             <h1 className="font-weight-light text-muted py-3">
-              {title} <small className="smaller" >{subheading}</small>
+              {title}
             </h1>
           </header>
         </div>
       </div>
+      {subheading && (
+        <div className="row">
+          <div className="col">
+            <p className="text-muted fst-italic mb-3">{subheading}</p>
+          </div>
+        </div>
+      )}
       {content}
     </>
   )
